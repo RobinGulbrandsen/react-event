4316d1f0ce100c5c18e7c4e7e783c6fba1fb411e
Robin Gulbrandsen <robin@123.no>
2016-10-04 14:37:41 -0700

    clean up code

--- src/components/ArticleListEntry.js
+++ src/components/ArticleListEntry.js
@@ -3,13 +3,3 @@ var ArticleListEntry = ({article, articleClickEvent}) => (
     <p onClick={() => articleClickEvent(article)}>{article.title}</p>
   </div>
 );
-
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-ArticleListEntry.propTypes = {
-  article: React.PropTypes.object.isRequired
-};
-
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-window.ArticleListEntry = ArticleListEntry;
